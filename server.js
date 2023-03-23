3f93ae06b675fca0c756a7bc225b8add487964fa
Syahir Suhairi <77099451+mdsyrz@users.noreply.github.com>
2023-03-23 09:26:37 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,39 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const fileUpload = require("express-fileupload");
-const flash = require("connect-flash");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var app = express();
+
+const methodOverride = require("method-override");
+const MongoStore = require("connect-mongo");
+
+const blogRoutes = require("./routes/blogs");
+const authRoutes = require("./routes/auth");
+
 const session = require("express-session");
-// Using Node.js `require()`
-const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 require("dotenv").config();
 require("./config/database");
 
-const MongoStore = require("connect-mongo");
+// view engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+
 const sessionStore = MongoStore.create({
   mongoUrl: process.env.DATABASE_URL,
-  collectionName: "session",
+  collectionName: "projecttwosession",
 });
-
-const newPostController = require("./controllers/newPost");
-const homeController = require("./controllers/home");
-const storePostController = require("./controllers/storePost");
-const getPostController = require("./controllers/getPost");
-
-const newUserController = require("./controllers/newUser");
-const storeUserController = require("./controllers/storeUser");
-const loginController = require("./controllers/login");
-const loginUserController = require("./controllers/loginUser");
-const logoutController = require("./controllers/logout");
-
-const validateMiddleware = require("./middleware/validationMiddleware");
-const authMiddleware = require("./middleware/authMiddleware");
-const redirectIfAuthenticatedMiddleware = require("./middleware/redirectIfAuthenticatedMiddleware");
-
-const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
@@ -53,62 +47,28 @@ app.use(
 
 app.use(flash());
 
-global.loggedIn = null;
-app.use("*", (req, res, next) => {
-  loggedIn = req.session.userId;
+// Initialize passport and sessions for storing users info
+//https: www.geeksforgeeks.org/node-js-authentication-using-passportjs-and-passport-local-mongoose/
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure passport to use locally
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
   next();
 });
 
-// middleware
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload());
-
-// validation/authentication controller
-app.use("/posts/store", validateMiddleware);
-app.get("/posts/new", authMiddleware, newPostController);
-app.post("/posts/store", storePostController);
-
-// post controller
-app.get("/", homeController);
-app.get("/post/:id", getPostController);
-
-// user controller
-app.get("/auth/register", redirectIfAuthenticatedMiddleware, newUserController);
-app.post(
-  "/users/register",
-  redirectIfAuthenticatedMiddleware,
-  storeUserController
-);
-app.get("/auth/login", redirectIfAuthenticatedMiddleware, loginController);
-app.post(
-  "/users/login",
-  redirectIfAuthenticatedMiddleware,
-  loginUserController
-);
-
-app.get("/auth/logout", logoutController);
-
-app.use((req, res) => res.render("notfound"));
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.render("./blogs/cover");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.use(blogRoutes);
+app.use(authRoutes);
 
 module.exports = app;
